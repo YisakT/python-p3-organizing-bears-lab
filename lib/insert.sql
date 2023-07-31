2cd7fa552b0d1ba1c4434c83edb84840db27cb55
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-31 17:18:01 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Bear1', 5, 'Male', 'Brown', 'Calm', 1),
+    ('Bear2', 3, 'Female', 'Black', 'Energetic', 1),
+    ('Bear3', 4, 'Male', 'White', 'Playful', 1),
+    ('Bear4', 2, 'Female', 'Golden', 'Aggressive', 0),
+    ('Bear5', 6, 'Male', 'Gray', 'Calm', 1),
+    ('Bear6', 7, 'Female', 'Brown', 'Energetic', 1),
+    ('Bear7', 4, 'Male', 'Black', 'Playful', 1),
+    (NULL, 3, 'Female', 'White', 'Aggressive', 0);
